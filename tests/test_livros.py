780eb2f3ce4eaa6f22dfd023dec03aea9f1010c3
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2021-10-02 11:13:16 -0300

    Curso "Testes em Python: trabalhando com dublês de testes" Aula 02 Tópico 05

--- tests/test_livros.py
+++ tests/test_livros.py
@@ -22,7 +22,7 @@ def test_consultar_livros_chama_preparar_dados_para_requisisao_uma_vez_e_com_os_
 @skip("Resolver problema do executar")
 def test_consultar_livros_chama_obter_url_usando_como_parametro_o_retorno_de_preparar_dados_para_requisicao():
     with patch("colecao.livros.preparar_dados_para_requisicao") as duble_preparar:
-        dados = {"author":"Agatha Christie"}
+        dados = {"author": "Agatha Christie"}
         duble_preparar.return_value = dados
         with patch("colecao.livros.obter_url") as duble:
             consultar_livros("Agatha Christie")
@@ -40,6 +40,7 @@ def test_consultar_livros_chama_executar_requisicao_usando_retorno_obter_url():
 
 class StubHTTPResponse:
 
+    # noinspection PyMethodMayBeStatic
     def read(self):
         return b""
 
@@ -48,17 +49,38 @@ class StubHTTPResponse:
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         pass
-#
-# def duble_url_open(url, timeout):
-#     return StubHTTPResponse()
-#
-# def test_executar_requisicao_retorna_tipo_string():
-#     with patch("colecao.livros.urlopen", duble_url_open):
-#         resultado = executar_requisicao("https://buscador?author=Jk+Rowlings")
-#         assert type(resultado) == str
 
-def test_executar_requisicao_retorna_tipo_string():
-    with patch("colecao.livros.urlopen") as duble_url_open:
-        duble_url_open.return_value = StubHTTPResponse()
+
+# noinspection PyUnusedLocal
+def duble_url_open(url, timeout):
+    return StubHTTPResponse()
+
+
+def test_executar_requisicao_retorna_tipo_string_test_com_stub_function():
+    with patch("colecao.livros.urlopen", duble_url_open):
+        resultado = executar_requisicao("https://buscador?author=Jk+Rowlings")
+        assert type(resultado) == str
+
+
+def test_executar_requisicao_retorna_tipo_string_test_com_stub_mock():
+    with patch("colecao.livros.urlopen") as duble_do_url_open:
+        duble_do_url_open.return_value = StubHTTPResponse()
         resultado = executar_requisicao("https://buscarlivros?author=Jk+Rowlings")
         assert type(resultado) == str
+
+
+def test_executar_requisicao_retorna_tipo_string_test_com_stub_return_value():
+    with patch("colecao.livros.urlopen", return_value=StubHTTPResponse()):
+        resultado = executar_requisicao("https://buscarlivros?author=Jk+Rowlings")
+        assert type(resultado) == str
+
+@patch("colecao.livros.urlopen", return_value=StubHTTPResponse())
+def test_executar_requisicao_retorna_tipo_string_test_com_decorador(duble_de_urlopen):
+    resultado = executar_requisicao("https://buscarlivros?author=Jk+Rowlings")
+    assert type(resultado) == str
+
+@patch("colecao.livros.urlopen")
+def test_executar_requisicao_retorna_tipo_string_test_com_decorador_uso_da_variavel(duble_de_urlopen):
+    duble_de_urlopen.return_value = StubHTTPResponse()
+    resultado = executar_requisicao("https://buscarlivros?author=Jk+Rowlings")
+    assert type(resultado) == str
