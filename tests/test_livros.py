e2163b0a3fead8b63398d1d20c894a180a60bce2
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2021-10-04 11:04:14 -0300

    Curso "Testes em Python: trabalhando com dublês de testes" Aula 03 Tópico 03

--- tests/test_livros.py
+++ tests/test_livros.py
@@ -9,39 +9,6 @@ from colecao.livros import (
     executar_requisicao
 )
 
-
-@skip("Vale quando consultar livros estiver completo")
-def test_consultar_livros_retorna_resultados_formato_string():
-    resultado = consultar_livros("Agatha Christie")
-    assert type(resultado) == str
-
-
-@skip("Resolver problema do executar")
-def test_consultar_livros_chama_preparar_dados_para_requisisao_uma_vez_e_com_os_mesmos_parametros_de_consultar_livros():
-    with patch("colecao.livros.preparar_dados_para_requisicao") as duble:
-        consultar_livros("Agatha Christie")
-        duble.assert_called_once_with("Agatha Christie")
-
-
-@skip("Resolver problema do executar")
-def test_consultar_livros_chama_obter_url_usando_como_parametro_o_retorno_de_preparar_dados_para_requisicao():
-    with patch("colecao.livros.preparar_dados_para_requisicao") as duble_preparar:
-        dados = {"author": "Agatha Christie"}
-        duble_preparar.return_value = dados
-        with patch("colecao.livros.obter_url") as duble:
-            consultar_livros("Agatha Christie")
-            duble.assert_called_once_with("https://buscador", dados)
-
-
-@skip("Resolver problema do executar")
-def test_consultar_livros_chama_executar_requisicao_usando_retorno_obter_url():
-    with patch("colecao.livros.obter_url") as duble_obter_url:
-        duble_obter_url.return_value = "https://buscador"
-        with patch("colecao.livros.executar_requisicao") as duble_executar_requisicao:
-            consultar_livros("Agatha Christie")
-            duble_executar_requisicao.assert_called_once_with("https://buscador")
-
-
 class StubHTTPResponse:
 
     # noinspection PyMethodMayBeStatic
@@ -56,20 +23,52 @@ class StubHTTPResponse:
 
 
 # noinspection PyUnusedLocal
-def duble_url_open(url, timeout):
+def stub_url_open(url, timeout):
     return StubHTTPResponse()
+
+
+@patch("colecao.livros.urlopen", return_value=StubHTTPResponse())
+def test_consultar_livros_retorna_resultados_formato_string(stub_urlopen):
+    resultado = consultar_livros("Agatha Christie")
+    assert type(resultado) == str
+
+
+@patch("colecao.livros.urlopen", return_value=StubHTTPResponse())
+def test_consultar_livros_chama_preparar_dados_para_requisisao_uma_vez_e_com_os_mesmos_parametros_de_consultar_livros(stub_urlopen):
+    with patch("colecao.livros.preparar_dados_para_requisicao") as spy_preparar_dados:
+        consultar_livros("Agatha Christie")
+        spy_preparar_dados.assert_called_once_with("Agatha Christie")
+
+
+@patch("colecao.livros.urlopen", return_value=StubHTTPResponse())
+def test_consultar_livros_chama_obter_url_usando_como_parametro_o_retorno_de_preparar_dados_para_requisicao(stub_urlopen):
+    with patch("colecao.livros.preparar_dados_para_requisicao") as stub_preparar:
+        dados = {"author": "Agatha Christie"}
+        stub_preparar.return_value = dados
+        with patch("colecao.livros.obter_url") as stub:
+            consultar_livros("Agatha Christie")
+            stub.assert_called_once_with("https://buscador", dados)
+
+
+@patch("colecao.livros.urlopen", return_value=StubHTTPResponse())
+def test_consultar_livros_chama_executar_requisicao_usando_retorno_obter_url(stub_urlopen):
+    with patch("colecao.livros.obter_url") as spy_obter_url:
+        spy_obter_url.return_value = "https://buscador"
+        with patch("colecao.livros.executar_requisicao") as spy_executar_requisicao:
+            consultar_livros("Agatha Christie")
+            spy_executar_requisicao.assert_called_once_with("https://buscador")
 
 
 def test_executar_requisicao_retorna_tipo_string_test_com_stub_function():
 
-    with patch("colecao.livros.urlopen", duble_url_open):
+    with patch("colecao.livros.urlopen", stub_url_open):
         resultado = executar_requisicao("https://buscador?author=Jk+Rowlings")
         assert type(resultado) == str
 
 
 def test_executar_requisicao_retorna_tipo_string_test_com_stub_mock():
-    with patch("colecao.livros.urlopen") as duble_do_url_open:
-        duble_do_url_open.return_value = StubHTTPResponse()
+    with patch("colecao.livros.urlopen") as stub_do_url_open:
+        stub_do_url_open.return_value = StubHTTPResponse()
         resultado = executar_requisicao("https://buscarlivros?author=Jk+Rowlings")
         assert type(resultado) == str
 
@@ -81,14 +80,14 @@ def test_executar_requisicao_retorna_tipo_string_test_com_stub_return_value():
 
 
 @patch("colecao.livros.urlopen", return_value=StubHTTPResponse())
-def test_executar_requisicao_retorna_tipo_string_test_com_decorador(duble_de_urlopen):
+def test_executar_requisicao_retorna_tipo_string_test_com_decorador(stub_de_urlopen):
     resultado = executar_requisicao("https://buscarlivros?author=Jk+Rowlings")
     assert type(resultado) == str
 
 
 @patch("colecao.livros.urlopen")
-def test_executar_requisicao_retorna_tipo_string_test_com_decorador_uso_da_variavel(duble_de_urlopen):
-    duble_de_urlopen.return_value = StubHTTPResponse()
+def test_executar_requisicao_retorna_tipo_string_test_com_decorador_uso_da_variavel(stub_de_urlopen):
+    stub_de_urlopen.return_value = StubHTTPResponse()
     resultado = executar_requisicao("https://buscarlivros?author=Jk+Rowlings")
     assert type(resultado) == str
 
@@ -96,11 +95,23 @@ def test_executar_requisicao_retorna_tipo_string_test_com_decorador_uso_da_varia
 class Dummy:
     pass
 
+
 def stub_url_open_que_levanta_excecao_http_error(url, timeout):
     fp = mock_open
     fp.close = Dummy()
     raise HTTPError(Dummy(), Dummy(), "mensagem de erro", Dummy(), fp)
+
 """
+def test_executar_requisicao_loga_mensagem_de_erro_de_http_error(caplog):
+    with patch("colecao.livros.urlopen", stub_url_open_que_levanta_excecao_http_error):
+        resultado = executar_requisicao("http://")
+        mensagem_de_erro = "mensagem de erro"
+        assert len(caplog.records) == 1
+
+        for registro in caplog.records:
+            assert mensagem_de_erro in registro.message
+
+
 def test_executar_requisicao_levanta_excecao_do_tipo_http_error():
     with patch("colecao.livros.urlopen", stub_url_open_que_levanta_excecao_http_error):
         with pytest.raises(HTTPError) as excecao:
@@ -110,34 +121,36 @@ def test_executar_requisicao_levanta_excecao_do_tipo_http_error():
 
 
 @patch("colecao.livros.urlopen")
-def test_executar_requisicao_levanta_excecao_do_tipo_http_error_com_anotacao(duble_de_urlopen):
+def test_executar_requisicao_levanta_excecao_do_tipo_http_error_com_anotacao(stub_de_urlopen):
     fp = mock_open
     fp.close = Dummy()
-    duble_de_urlopen.side_effect = HTTPError(Dummy(), Dummy(), "mensagem de erro", Dummy(), fp)
+    stub_de_urlopen.side_effect = HTTPError(Dummy(), Dummy(), "mensagem de erro", Dummy(), fp)
     with pytest.raises(HTTPError) as excecao:
         executar_requisicao("https://buscarlivros?author=Jk+Rowlings")
         assert "mensagem de erro" in str(excecao.value)
 
 @patch("colecao.livros.urlopen")
-def test_executar_requisicao_levanta_excecao_do_tipo_http_error_com_anotacaoe_mock(duble_de_urlopen):
+def test_executar_requisicao_levanta_excecao_do_tipo_http_error_com_anotacaoe_mock(stub_de_urlopen):
 
     fp = mock_open
     fp.close = Mock()
-    duble_de_urlopen.side_effect = HTTPError(Mock(), Mock(), "mensagem de erro", Mock(), fp)
+    stub_de_urlopen.side_effect = HTTPError(Mock(), Mock(), "mensagem de erro", Mock(), fp)
     with pytest.raises(HTTPError) as excecao:
         executar_requisicao("https://buscarlivros?author=Jk+Rowlings")
         assert "mensagem de erro" in str(excecao.value)
 """
+@patch("colecao.livros.urlopen")
+def test_executar_requisicao_loga_mensagem_de_erro_de_http_error(stub_de_urlopen, caplog):
+    fp = mock_open
+    fp.close = Mock()
+    stub_de_urlopen.side_effect = HTTPError(Mock(), Mock(), "mensagem de erro", Mock(), fp)
+
+    executar_requisicao("http://")
+    assert len(caplog.records) == 1
+    for registro in caplog.records:
+        assert "mensagem de erro" in registro.message
 
 
-def test_executar_requisicao_loga_mensagem_de_erro_de_http_error(caplog):
-    with patch("colecao.livros.urlopen", stub_url_open_que_levanta_excecao_http_error):
-        resultado = executar_requisicao("http://")
-        mensagem_de_erro = "mensagem de erro"
-        assert len(caplog.records) == 1
-
-        for registro in caplog.records:
-            assert mensagem_de_erro in registro.message
 
 
 
